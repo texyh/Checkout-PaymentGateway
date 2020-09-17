6c1f6a8f1d6fb2442b9de4939003a7fbb3519021
Onwuzulike Emeka <onwuzulikee1@gmail.com>
2020-09-17 08:48:52 +0200

    Added Swagger

--- src/PaymentGateway.Api/Startup.cs
+++ src/PaymentGateway.Api/Startup.cs
@@ -22,19 +22,25 @@ namespace PaymentGateway.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services
+                .AddSwaggerGen()
+                .AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+            });
 
             app.UseHttpsRedirection();
 
---
+++ src/PaymentGateway.Api/StartupExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Api
+{
+    public static class StartupExtensions
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Payment Gateway Api",
+                    Version = "v1",
+                    Description = "This is an api that allow merchants process and manage payments"
+                });
+            });
+
+            return services;
+        }
+    }
+}
