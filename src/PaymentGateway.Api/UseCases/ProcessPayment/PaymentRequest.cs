fa0d462afaf58ce2b36b063f5cec846f84e4bed7
Onwuzulike Emeka <onwuzulikee1@gmail.com>
2020-10-14 10:47:12 +0200

    Added validation for process payment command

--- src/PaymentGateway.Api/UseCases/ProcessPayment/PaymentRequest.cs
+++ src/PaymentGateway.Api/UseCases/ProcessPayment/PaymentRequest.cs
@@ -8,26 +8,18 @@ namespace PaymentGateway.Api.UseCases.ProcessPayment
 {
     public class PaymentRequest
     {
-        [Required]
-        [MinLength(16)]
         public string CardNumber { get; set; }
 
-        [Required]
         public string CardExpiryMonth { get; set; }
 
-        [Required]
         public string CardExpiryYear { get; set; }
 
-        [Required]
         public decimal Amount { get; set; }
 
-        [Required]
         public string Currency { get; set; }
 
-        [Required]
         public string CVV { get; set; }
 
-        [Required]
         public string MerchantId { get; set; }
     }
 }
--- src/PaymentGateway.Application/Payments/ProcessPayment/ProcessPaymentCommandValidator.cs
+++ src/PaymentGateway.Application/Payments/ProcessPayment/ProcessPaymentCommandValidator.cs
@@ -2,15 +2,34 @@
 using PaymentGateway.Domain.Payments.Commands;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace PaymentGateway.Application.Payments.ProcessPayment
 {
     public class ProcessPaymentCommandValidator : AbstractValidator<ProcessPaymentCommand>
     {
+        private const byte MinimumAmount = 0;
+        private  string[] AcceptedCurrencies = new string[] { "EUR", "USD" };
         public ProcessPaymentCommandValidator()
         {
-            // TODO
+            RuleFor(x => x.CardNumber).NotNull().NotEmpty().CreditCard();
+            RuleFor(x => x.Amount).NotEmpty().GreaterThan(MinimumAmount).WithMessage("amount must be greater than 0");
+            RuleFor(x => x.Currency).NotNull().Must(x => AcceptedCurrencies.Contains(x)).WithMessage("The currency is not support");
+            RuleFor(x => x.CardExpiryYear).NotEmpty().NotNull().Must((x, y) => ValidateCardExpiryDate(x)).WithMessage("The credit card is expired");
+            RuleFor(x => x.CVV).NotEmpty().NotNull();
+            RuleFor(x => x.MerchantId).NotNull().NotEmpty();
+            RuleFor(x => x.CardExpiryYear).NotEmpty().NotNull().Must(x => int.TryParse(x, out var value)).WithMessage("expiry year is not valid");
+            RuleFor(x => x.CardExpiryMonth).NotEmpty().NotNull().Must(x => int.TryParse(x, out var value)).WithMessage("expiry month is not valid");
+
+        }
+
+        private bool ValidateCardExpiryDate(ProcessPaymentCommand command)
+        {
+            var isValidYear = int.TryParse(command.CardExpiryYear, out var year);
+            var isValidMonth = int.TryParse(command.CardExpiryMonth, out var month);
+
+            return (isValidYear && year >= DateTime.UtcNow.Year && isValidMonth && month > DateTime.UtcNow.Month);
         }
     }
 }
